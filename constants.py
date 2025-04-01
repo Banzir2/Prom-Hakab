78a2a47de4545d4a3906291950dc2bf2b303f4c6
Banzir2 <eitan.vichik.46@gmail.com>
2025-04-01 21:19:26 +0300

    Get started broda

---
+++ constants.py
@@ -0,0 +1,12 @@
+import math
+
+min_height = 15000  # Meters
+max_height = 20000  # Meters
+small_range = 60000  # Meters
+large_range = 80000  # Meters
+
+earth_radius = 6378000  # Meters
+
+detection_lambda = 3
+
+
---
+++ functions.py
@@ -0,0 +1,21 @@
+import constants as c
+import math
+
+
+def detection_probability_cu(time: float) -> float:
+    return 1 - math.exp(-c.detection_lambda * time)
+
+
+def area_cut_circle(r: float, y_max: float) -> float:
+    r2 = r ** 2
+    a = math.sqrt(r2 - y_max ** 2)
+    return y_max * a + r2 * math.atan(y_max / a)
+
+
+def expected_time(v: float, r: float, t_min: float) -> float:
+    y_max = math.sqrt(r ** 2 - (v * t_min / 2) ** 2)
+    return (1 / (v * r)) * area_cut_circle(r, y_max)
+
+
+def prob_detection(v: float, r: float, t_min: float) -> float:
+    return detection_probability_cu(expected_time(v, r, t_min))
