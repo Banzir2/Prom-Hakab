e0396e5c6d5242faafdaca63e0f9a6b4765866eb
Banzir2 <eitan.vichik.46@gmail.com>
2025-04-28 15:56:36 +0300

    Interpolate between points

--- create_balloon_config.py
+++ create_balloon_config.py
@@ -10,12 +10,18 @@ import functions
 if __name__ == '__main__':
     warnings.filterwarnings("ignore")
     df = pd.read_csv("balloon_interpolates.csv")
-    interpolates = df.values
+    interpolates = list(df.values)
     positions = [np.array(interpolates[0])]
+    interpolates.append(interpolates[0])
     for i in range(len(interpolates)):
         if i == 0:
             continue
         diff = np.array(interpolates[i]) - np.array(interpolates[i-1])
         step = math.degrees(500 / constants.earth_radius)
         step_vec = (diff / functions.vec_length(diff)) * step
+        pos = np.array(interpolates[i-1])
+        while functions.vec_length(diff) > functions.vec_length(step_vec):
+            pos += step_vec
+            positions.append(pos)
+            diff -= step_vec
 
