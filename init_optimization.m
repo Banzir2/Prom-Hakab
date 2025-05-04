d916f5c60e4228fe03b5f4cb734311b849db1e3b
Banzir2 <eitan.vichik.46@gmail.com>
2025-05-04 11:09:17 +0300

    Separate init and optimization

---
+++ init_optimization.m
@@ -0,0 +1,26 @@
+close all; clear;
+
+borders = readmatrix('borders.csv');
+
+maxX = 823181.7714890691;
+maxY = 3901511.0344994236;
+minX = 565998.3597003543;
+minY = 3064491.748778477;
+
+X = linspace(minX, maxX, (maxX - minX) / 500).';
+Y = linspace(minY, maxY, (maxY - minY) / 500).';
+[X, Y] = meshgrid(X, Y);
+thicc_border = zeros(size(X));
+
+inpolygonmatrix = inpolygon(X, Y, borders(:,1), borders(:,2));
+for b=borders.'
+    R = sqrt((X - b(1)).^2 + (Y - b(2)).^2);
+    thicc_border(R < 40000 & (inpolygonmatrix == 1)) = 1;
+    thicc_border(R < 40000 & (~(inpolygonmatrix == 1))) = 1;
+end
+
+sizes = size(thicc_border);
+
+idx = find(thicc_border==1);
+points(:,1) = X(idx);
+points(:,2) = Y(idx);
--- optimize_balloons.m
+++ optimize_balloons.m
@@ -1,30 +1,3 @@
-close all; clear;
-
-borders = readmatrix('borders.csv');
-
-maxX = 823181.7714890691;
-maxY = 3901511.0344994236;
-minX = 565998.3597003543;
-minY = 3064491.748778477;
-
-X = linspace(minX, maxX, (maxX - minX) / 500).';
-Y = linspace(minY, maxY, (maxY - minY) / 500).';
-[X, Y] = meshgrid(X, Y);
-thicc_border = zeros(size(X));
-
-inpolygonmatrix = inpolygon(X, Y, borders(:,1), borders(:,2));
-for b=borders.'
-    R = sqrt((X - b(1)).^2 + (Y - b(2)).^2);
-    thicc_border(R < 40000 & (inpolygonmatrix == 1)) = 1;
-    thicc_border(R < 40000 & (~(inpolygonmatrix == 1))) = 1;
-end
-
-sizes = size(thicc_border);
-
-idx = find(thicc_border==1);
-points(:,1) = X(idx);
-points(:,2) = Y(idx);
-
 balloons = [[642106.7687921221, 3498466.1847251933];
      [671012.5189409455, 3570217.4580244776];
      [671012.5189409455, 3570217.4580244776];
