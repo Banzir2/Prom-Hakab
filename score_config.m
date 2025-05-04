2c7f44911023440dd51236238760cbbc3d959ec1
Banzir2 <eitan.vichik.46@gmail.com>
2025-05-04 19:49:44 +0300

    Change score function for optimization

--- score_config.m
+++ score_config.m
@@ -8,14 +8,8 @@ function [out] = score_config(balloons, points)
         dist = sqrt(diffx.^2 + diffy.^2);
         binary_dists = zeros(rc2(1), 1);
         binary_dists(dist < 56500) = 1;
-        total = total + binary_dists;
+        total = total | binary_dists;
     end
-    [bx, by] = meshgrid(balloons(:,1), balloons(:,2));
-    distx = bx - bx.';
-    disty = by - by.';
-    R = sqrt(distx.^2 + disty.^2);
-    R = R(R > 0);
-    min_dist = min(R);
-    out = -sum(sum(sqrt(sqrt(total)))) * log(min_dist);
+    out = -sum(sum(total));
 end
 
